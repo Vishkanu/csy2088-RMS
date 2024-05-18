efc2bc1811ea1cc6d629a9b64a749ec9ca338166
Arakimo <mail@arakimo.net>
2024-05-18 19:04:32 +0100

    db: schema tweaks + fixed inserts

--- db/insert.sql
+++ db/insert.sql
@@ -7,35 +7,35 @@ VALUES (22400000, 'FOO', 'BAR', 'FOO@BAR.COM', 'L');
 
 --course table:
 INSERT INTO `csy2088_as1`.`courses` (`course_id`, `course_name`) 
-VALUES ('1', 'Computer Science');
+VALUES ('70000001', 'Computer Science');
 
 INSERT INTO `csy2088_as1`.`courses` (`course_id`, `course_name`) 
-VALUES ('2', 'Software Engineering');
+VALUES ('70000002', 'Software Engineering');
 
 INSERT INTO `csy2088_as1`.`courses` (`course_id`, `course_name`) 
-VALUES ('3', 'Medicine');
+VALUES ('70000003', 'Medicine');
 
 INSERT INTO `csy2088_as1`.`courses` (`course_id`, `course_name`) 
-VALUES ('4', 'Art');
+VALUES ('70000004', 'Art');
 
 INSERT INTO `csy2088_as1`.`courses` (`course_id`, `course_name`) 
-VALUES ('5', 'English Language Studies');
+VALUES ('70000005', 'English Language Studies');
 
 -- students table:
 INSERT INTO `csy2088_as1`.`students` 
-VALUES('22400001', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 1, 'A A B', 99100000);
+VALUES('22400001', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 70000001, 'A A B', 99100000);
 
 INSERT INTO `csy2088_as1`.`students` 
-VALUES('22400002', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 1, 'A A B', 99100000);
+VALUES('22400002', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 70000001, 'A A B', 99100000);
 
 INSERT INTO `csy2088_as1`.`students` 
-VALUES('22400003', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 1, 'A A B', 99100000);
+VALUES('22400003', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 70000001, 'A A B', 99100000);
 
 INSERT INTO `csy2088_as1`.`students` 
-VALUES('22400004', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 1, 'A A B', 99100000);
+VALUES('22400004', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 70000001, 'A A B', 99100000);
 
 INSERT INTO `csy2088_as1`.`students` 
-VALUES('22400005', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 1, 'A A B', 99100000);
+VALUES('22400005', 'John', 'Long', 'Doe', '89 Grange Park', '90 Park Lane',  '07567901245','johndoe@gmail.com', '$2y$10$RJP5nezpfuwIGBTvqm86geJKzrERjdGIqpON8zRhjO7XFnWheywvu', 'L', NULL, 70000001, 'A A B', 99100000);
 
 -- modules table
 INSERT INTO modules
@@ -61,25 +61,25 @@ VALUES ('C3002', 1, 20, 'Buzzword Engineering', NULL, NULL, 100, 99100000);
 
 -- course_modules table
 INSERT INTO course_modules
-VALUES (1, 'C1001');
+VALUES (70000001, 'C1001');
 
 INSERT INTO course_modules
-VALUES (1, 'C1002');
+VALUES (70000001, 'C1002');
 
 INSERT INTO course_modules
-VALUES (2, 'C1003');
+VALUES (70000002, 'C1003');
 
 INSERT INTO course_modules
-VALUES (1, 'C2001');
+VALUES (70000001, 'C2001');
 
 INSERT INTO course_modules
-VALUES (1, 'C2002');
+VALUES (70000001, 'C2002');
 
 INSERT INTO course_modules
-VALUES (1, 'C3001');
+VALUES (70000001, 'C3001');
 
 INSERT INTO course_modules
-VALUES (1, 'C3002');
+VALUES (70000001, 'C3002');
 
 -- rooms table
 INSERT INTO rooms
