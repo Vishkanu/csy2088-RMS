025e338186e60aad27ae45e964c9c2a21dcd6ca0
Arakimo <mail@arakimo.net>
2024-05-20 15:24:18 +0100

    added welcome message to dashboard

---
+++ container/websites/rms/templates/home.html.php
@@ -0,0 +1,14 @@
+<style>
+.center-div {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10%;
+  text-align: center;
+  width: 50%;
+
+}
+</style>
+
+<div class="center-div">
+<h1>Welcome to the Woodlands University - Records Management System</h1>
+</div>
