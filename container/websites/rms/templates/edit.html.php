0d259652343e9718d5d4d23af5e04cbc40a05b7f
Michael Leggett <126602573+Vishkanu@users.noreply.github.com>
2024-05-20 16:25:07 +0100

    shouldve moved to a branch but fixed forms into columns!

--- container/websites/rms/templates/edit.html.php
+++ container/websites/rms/templates/edit.html.php
@@ -15,34 +15,39 @@ foreach (array_keys($userRecord) as $key) {
 ?>
 
 <div class="col-md-10 py-4">
-<h1>Edit Record</h1>
-<?php
-// TODO: PRESERVE $_GET VARIABLES ON FORM SUBMISSION
-?>
-<form method="POST" action="edit?<?=$_SERVER['QUERY_STRING'];?>">
-<?php
-for ($i = 0; $i < sizeof($fieldNames); $i++) { ?>
-	<label for="<?=$fieldNames[$i];?>"><?=ucwords(str_replace('_',' ',$fieldNames[$i]));?></label><br>
-	<input type="text" id=<?=$fieldNames[$i];?> name=<?=$fieldNames[$i];?> value="<?=$userRecord[$i];?>" <?=$inputOpts[$i];?> ><br>
-<?php
-}
-?>
-<input type="submit" name="submit" class="btn btn-purple btn-block">
-<button type="button" onclick="javascript:history.back()" class="btn btn-purple btn-block">Go Back</button>
-</form>
+	<h1>Edit Record</h1>
+	<?php
+	// TODO: PRESERVE $_GET VARIABLES ON FORM SUBMISSION
+	?>
+	<form method="POST" action="edit?<?= $_SERVER['QUERY_STRING']; ?>">
+		<?php
+		for ($i = 0; $i < sizeof($fieldNames); $i++) { ?>
+			<div class="col-md-6 mb-3">
+				<label for="<?= $fieldNames[$i]; ?>"><?= ucwords(str_replace('_', ' ', $fieldNames[$i])); ?></label>
+				<input type="text" id="<?= $fieldNames[$i]; ?>" name="<?= $fieldNames[$i]; ?>" class="form-control"
+					value="<?= $userRecord[$i]; ?>" <?= $inputOpts[$i]; ?>>
+			</div>
+			<?php
+		}
+		?>
+		<div class="form-group text-center">
+			<input type="submit" name="submit" class="btn btn-purple btn-block">
+			<button type="button" onclick="javascript:history.back()" class="btn btn-purple btn-block">Go Back</button>
+		</div>
 
-<?php
-if (isset($hasPassword) && $hasPassword == true) {
-?>
-<form method="POST" action="edit?<?=$_SERVER['QUERY_STRING'];?>">
-	<label for="change_password">Password Reset:</label><br>
-	<input type="text" id="change_password" name="change_password" class="btn btn-purple btn-block"><br>
-<input type="submit" name="submit2" class="btn btn-purple btn-block">
-</form>
-<?php
-}
-?>
+	</form>
+
+	<?php
+	if (isset($hasPassword) && $hasPassword == true) {
+		?>
+		<form method="POST" action="edit?<?= $_SERVER['QUERY_STRING']; ?>">
+			<label for="change_password">Password Reset:</label><br>
+			<input type="text" id="change_password" name="change_password" class="btn btn-purple btn-block"><br>
+			<input type="submit" name="submit2" class="btn btn-purple btn-block">
+		</form>
+		<?php
+	}
+	?>
 
 
 </div>
-
