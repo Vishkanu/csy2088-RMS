bd1f7a2ad4b56759faf0bbb4c9baaf98e95df301
Michael Leggett <126602573+Vishkanu@users.noreply.github.com>
2024-05-20 16:28:38 +0100

    trying again!

--- container/websites/rms/templates/edit.html.php
+++ container/websites/rms/templates/edit.html.php
@@ -19,35 +19,37 @@ foreach (array_keys($userRecord) as $key) {
 	<?php
 	// TODO: PRESERVE $_GET VARIABLES ON FORM SUBMISSION
 	?>
-	<form method="POST" action="edit?<?= $_SERVER['QUERY_STRING']; ?>">
-		<?php
-		for ($i = 0; $i < sizeof($fieldNames); $i++) { ?>
-			<div class="col-md-6 mb-3">
-				<label for="<?= $fieldNames[$i]; ?>"><?= ucwords(str_replace('_', ' ', $fieldNames[$i])); ?></label>
-				<input type="text" id="<?= $fieldNames[$i]; ?>" name="<?= $fieldNames[$i]; ?>" class="form-control"
-					value="<?= $userRecord[$i]; ?>" <?= $inputOpts[$i]; ?>>
+	<div class="container mt-5">
+		<form method="POST" action="edit?<?= $_SERVER['QUERY_STRING']; ?>">
+			<div class="row">
+				<?php
+				for ($i = 0; $i < sizeof($fieldNames); $i++) { ?>
+					<div class="col-md-6 mb-3">
+						<label for="<?= $fieldNames[$i]; ?>"><?= ucwords(str_replace('_', ' ', $fieldNames[$i])); ?></label>
+						<input type="text" id="<?= $fieldNames[$i]; ?>" name="<?= $fieldNames[$i]; ?>" class="form-control"
+							value="<?= $userRecord[$i]; ?>" <?= $inputOpts[$i]; ?>>
+					</div>
+					<?php
+				}
+				?>
 			</div>
+			<div class="form-group text-center">
+				<input type="submit" name="submit" class="btn btn-purple">
+				<button type="button" onclick="javascript:history.back()" class="btn btn-purple">Go Back</button>
+			</div>
+
+		</form>
+
+		<?php
+		if (isset($hasPassword) && $hasPassword == true) {
+			?>
+			<form method="POST" action="edit?<?= $_SERVER['QUERY_STRING']; ?>">
+				<label for="change_password">Password Reset:</label><br>
+				<input type="text" id="change_password" name="change_password"><br>
+				<input type="submit" name="submit2" class="btn btn-purple">
+			</form>
 			<?php
 		}
 		?>
-		<div class="form-group text-center">
-			<input type="submit" name="submit" class="btn btn-purple btn-block">
-			<button type="button" onclick="javascript:history.back()" class="btn btn-purple btn-block">Go Back</button>
-		</div>
-
-	</form>
-
-	<?php
-	if (isset($hasPassword) && $hasPassword == true) {
-		?>
-		<form method="POST" action="edit?<?= $_SERVER['QUERY_STRING']; ?>">
-			<label for="change_password">Password Reset:</label><br>
-			<input type="text" id="change_password" name="change_password" class="btn btn-purple btn-block"><br>
-			<input type="submit" name="submit2" class="btn btn-purple btn-block">
-		</form>
-		<?php
-	}
-	?>
-
-
+	</div>
 </div>
